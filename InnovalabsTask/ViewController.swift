3e8283a7c4a01a4ea631e862d8cc4d2dcb87636b
Kshitija Palande <kshitijapalande123@gmail.com>
2022-11-30 22:10:17 +0530

    Initial Commit

---
+++ InnovalabsTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InnovalabsTask
+//
+//  Created by Kshitija Palande on 30/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
